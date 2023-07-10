436e527ef94a3e46ac8065969867769d716e6b4e
kristendecastro <133027732+kristendecastro@users.noreply.github.com>
2023-07-10 21:24:33 +0800

    Add files via upload

---
+++ LANDING-PAGE.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html> 
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Homepage</title>
+	<link rel="stylesheet" type="text/css" href="landing-page.css">
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	
+	<link rel="icon" type="image/x-icon" href=">
+</head>
+<body>
+
+<!--NAVIGATION BAR-->
+	<nav>
+    <div class="menu">
+      <div class="logo">
+        <a><img class="logo-icon" src="https://64.media.tumblr.com/8aefa2dbbd2867ebd47ae1b76fafc693/8a037a56b486f59a-40/s640x960/2296eaf1d86edd1f12ac4c2a88c3016a07ec3342.pnj" alt="luzon">Luzon</a>
+      </div>
+      <ul>
+		<!--HOMEPAGE-->
+        <li><a href="index.html">Home</a></li>
+		
+		<!-- LECTURE DROPDOWN -->
+		<li class="dropdown">
+			<a href="index.html" class="dropdown-toggle" data-toggle="dropdown">Lecture <span class="caret"></span></a>
+			<ul class="dropdown-menu">
+				<li><a href="#" target="_blank">LECT1: Intro to PHP</a></li>
+				<li><a href="#" target="_blank">LECT2: Functions & control Structures</a></li>
+				<li><a href="#" target="_blank">LECT3: String Functions</a></li>
+				<li><a href="#" target="_blank">LECT4: User Input</a></li>
+				<li><a href="#" target="_blank">LECT5: Array Functions</a></li>
+				<li><a href="#" target="_blank">LECT6: MySQL</a></li>
+			</ul>
+		</li>
+
+		<!-- MEMBER 1 ACTIVITIES DROPDOWN -->
+		<li class="dropdown">
+			<a href="index.html" class="dropdown-toggle" data-toggle="dropdown">Member 1 <span class="caret"></span></a>
+			<ul class="dropdown-menu">
+				<li><a href="#" target="_blank">Midterm Act 1</a></li>
+				<li><a href="#" target="_blank">Midterm Act 2</a></li>
+				<li><a href="#" target="_blank">Midterm Act 3</a></li>
+				<li><a href="#" target="_blank">Midterm Quiz 2</a></li>
+				<li><a href="#" target="_blank">Midterm Exam</a></li>
+				<li><a href="#" target="_blank">Finals Act 1</a></li>
+			</ul>
+		</li>
+		
+		<!-- MEMBER 2 ACTIVITIES DROPDOWN -->
+		<li class="dropdown">
+			<a href="index.html" class="dropdown-toggle" data-toggle="dropdown">Member 2<span class="caret"></span></a>
+			<ul class="dropdown-menu">
+				<li><a href="#" target="_blank">Midterm Act 1</a></li>
+				<li><a href="#" target="_blank">Midterm Act 2</a></li>
+				<li><a href="#" target="_blank">Midterm Act 3</a></li>
+				<li><a href="#" target="_blank">Midterm Quiz 2</a></li>
+				<li><a href="#" target="_blank">Midterm Exam</a></li>
+				<li><a href="#" target="_blank">Finals Act 1</a></li>
+			</ul>
+		</li>
+		
+		<!-- MINI PROJ. DROPDOWN -->
+		<li class="dropdown">
+			<a href="index.html" class="dropdown-toggle" data-toggle="dropdown">Celestia<span class="caret"></span></a>
+			<ul class="dropdown-menu">
+				<li><a href="#" target="_blank">Homepage</a></li>
+				<li><a href="#" target="_blank">Login</a></li>
+				<li><a href="#" target="_blank">Content</a></li>
+				<li><a href="#" target="_blank">Courses</a></li>
+				<li><a href="#" target="_blank">Contact</a></li>
+				<li><a href="#" target="_blank">Finals Act 1</a></li>
+			</ul>
+		</li>
+	
+      </ul>
+	  
+    </div>
+	</nav>
